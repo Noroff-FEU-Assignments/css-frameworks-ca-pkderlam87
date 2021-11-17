706009a78f2e2cfb449076116b07dbd5fb3f9283
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2021-11-17 11:33:30 +0100

    Added form to a contact

--- yay/src/components/Contact/FormContact.js
+++ yay/src/components/Contact/FormContact.js
@@ -6,21 +6,34 @@ function FormContact() {
     return (
         <>
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="name" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Email</Form.Label>
                     <Form.Control type="email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Website</Form.Label>
+                    <Form.Control type="url" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" rows={6} />
+                </Form.Group>
+                <Form.Group>
+                    {['checkbox'].map((type) => (
+                        <div key={type} className="mb-3">
+                            <Form.Check type={type} id={`check-api-${type}`}>
+                                <Form.Check.Input type={type} isValid />
+                                <Form.Check.Label>{`Allow us to sell your personal details
+to whomever we want`}</Form.Check.Label>
+                            </Form.Check>
+                        </div>
+                    ))}
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
